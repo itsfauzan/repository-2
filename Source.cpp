5a21fa129da8b478bd9f9aa92ba497874ef3672a
itsfauzan <fauzanyahya@students.amikom.ac.id>
2023-03-21 00:07:23 +0700

    test

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Tone Genertaor. Type 'q' to quit...";
+
+	while (true)
+	{
+		double freq = 0.0;
+		char tone = _getch();
+		switch (tone)
+		{
+		case 'c' : freq =
+		}
+	}
+}
